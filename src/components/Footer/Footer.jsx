858fc1dcd0789e86e16157f052eca40f7757666e
Rion Dsilva <118284932+RionDsilvaCS@users.noreply.github.com>
2024-02-22 16:23:02 +0530

    footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import './Footer.scss'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className='main-footer'>
+      <div className='footer-box'> 
+      <h1>JobVista</h1>
+      <p>© 2024 JobVista.in<br></br>
+      All rights reserved</p>
+      </div>
+      <div className='footer-box'>
+      <h3>Quick links</h3>
+      <p>Web page<br></br>
+      Online shop<br></br>
+      Counseling<br></br></p>
+      
+      </div>
+      <div className='footer-box'>
+      <h3>Online courses</h3>
+      <p>Bachelors<br></br>
+      Masters<br></br>
+      Jobs & Research<br></br></p>
+      </div>
+    </div>
+    </>
   )
 }
 
